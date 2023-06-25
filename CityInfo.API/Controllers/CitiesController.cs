b8ef045670f05641bc6372e938b1b11fc90c292f
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-06-25 13:37:08 +0100

    add API to create a resource

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+	[ApiController]
+	[Route("api/cities")]
+	public class CitiesController: ControllerBase
+	{
+		[HttpGet]
+		public ActionResult<IEnumerable<CityDto>> GetCities()
+		{
+			return Ok(CitiesDataStore.Current.Cities);
+		}
+
+		[HttpGet("{id}")]
+		public ActionResult<CityDto> GetCity(int id)
+		{
+			// Find City
+			var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(city => city.Id == id);
+
+			if (cityToReturn == null)
+			{
+				return NotFound();
+			}
+
+            return Ok(cityToReturn);
+		}
+	}
+}
+
