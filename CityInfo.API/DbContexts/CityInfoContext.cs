d330fad57132cb078de481844697dc988fb4d6ad
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-06-26 13:11:22 +0100

    add DbContexts

---
+++ CityInfo.API/DbContexts/CityInfoContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.DbContexts
+{
+	public class CityInfoContext: DbContext
+	{
+		public DbSet<City> City { get; set; } = null!;
+
+		public DbSet<PointOfInterest> PointsOfInterest { get; set; } = null!;
+
+		public CityInfoContext(DbContextOptions<CityInfoContext> options)
+			: base(options)
+		{
+
+		}
+	}
+}
+
--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,4 +1,6 @@
 ﻿using CityInfo.API;
+using CityInfo.API.DbContexts;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,9 @@ builder.Services.AddControllers(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<CitiesDataStore>();
+
+builder.Services.AddDbContext<CityInfoContext>(
+    dbContextOptions => dbContextOptions.UseSqlite("Data Source=CityInfo.db"));
 
 var app = builder.Build();
 
