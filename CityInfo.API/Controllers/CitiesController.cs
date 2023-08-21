6d0c83e159b2ea8ec94c08b8c4f007aa7825e097
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-08-21 14:25:59 +0100

    describe response type and status code for docs

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -51,8 +51,12 @@ namespace CityInfo.API.Controllers
 		/// <param name="id">The id of the city to get</param>
 		/// <param name="includePointsOfInterest">Whether or not to include the points of interest</param>
 		/// <returns>An IActionResult</returns>
+		/// <response code="200">Returns the requested city</response>
 		[HttpGet("{id}")]
-		public async Task<ActionResult> GetCity(
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> GetCity(
 			int id, bool includePointsOfInterest = false)
 		{
 			// Find City
