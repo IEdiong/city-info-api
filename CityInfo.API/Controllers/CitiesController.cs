b919fd67a1187e6360f734f528f0caf008e5cc60
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-08-21 10:55:25 +0100

    implement pagination
    - return pagination metadata

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using System.Text.Json;
+using AutoMapper;
 using CityInfo.API.Models;
 using CityInfo.API.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +12,7 @@ namespace CityInfo.API.Controllers
 	{
         private readonly ICityInfoRepository _cityInfoRepository;
         private readonly IMapper _mapper;
+		const int maxCitiesPageSize = 20;
 
         public CitiesController(ICityInfoRepository cityInfoRepository,
 			IMapper mapper)
@@ -23,9 +25,19 @@ namespace CityInfo.API.Controllers
 
 		[HttpGet]
 		public async Task<ActionResult<IEnumerable<CityWithoutPointsOfInterestDto>>> GetCities(
-			string? name, string? searchQuery)
+			string? name, string? searchQuery, int pageNumber = 1, int pageSize = 10)
 		{
-			var cityEntities = await _cityInfoRepository.GetCitiesAsync(name, searchQuery);
+			if (pageSize > maxCitiesPageSize)
+			{
+				pageSize = maxCitiesPageSize;
+			}
+
+			var (cityEntities, paginationMetadata ) = await _cityInfoRepository
+				.GetCitiesAsync(name, searchQuery, pageNumber, pageSize);
+
+			Response.Headers.Add("X-Pagination",
+				JsonSerializer.Serialize(paginationMetadata));
+
 			return Ok(_mapper.Map<IEnumerable<CityWithoutPointsOfInterestDto>>(cityEntities));
 		}
 
--- CityInfo.API/Services/CityInfoRepository.cs
+++ CityInfo.API/Services/CityInfoRepository.cs
@@ -19,15 +19,9 @@ namespace CityInfo.API.Services
                 .ToListAsync();
         }
 
-        public async Task<IEnumerable<City>> GetCitiesAsync(
-            string? name, string? searchQuery)
+        public async Task<(IEnumerable<City>, PaginationMetadata)> GetCitiesAsync(
+            string? name, string? searchQuery, int pageNumber, int pageSize)
         {
-            if (string.IsNullOrEmpty(name)
-                && string.IsNullOrWhiteSpace(searchQuery))
-            {
-                return await GetCitiesAsync();
-            }
-
             // collection to start from
             var collection = _context.Cities as IQueryable<City>;
 
@@ -44,7 +38,17 @@ namespace CityInfo.API.Services
                 || (a.Description != null && a.Description.Contains(searchQuery)));
             }
 
-            return await collection.OrderBy(c => c.Name).ToListAsync();
+            var totalItemCount = await collection.CountAsync();
+
+            var paginationMetadata = new PaginationMetadata(
+                totalItemCount, pageSize, pageNumber);
+
+            var collectionToReturn = await collection.OrderBy(c => c.Name)
+                .Skip(pageSize * (pageNumber - 1))
+                .Take(pageSize)
+                .ToListAsync();
+
+            return (collectionToReturn, paginationMetadata);
         }
 
         public async Task<City?> GetCityAsync(int cityId,
